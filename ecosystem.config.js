9fb679da71da600a43f86a506d4f40892b793273
xyukuri <xyukuri@outlook.jp>
2026-07-07 00:42:41 +0800

    packages

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    apps: [
+        {
+            name:
+                "Kyouroga-Bridge-Git",
+
+            script:
+                "./src/app.js",
+
+            instances: 1,
+
+            autorestart: true,
+
+            watch: false,
+
+            max_memory_restart:
+                "256M",
+
+            env: {
+                NODE_ENV:
+                    "production"
+            }
+        }
+    ]
+};
