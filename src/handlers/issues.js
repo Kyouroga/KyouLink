78240f57b94703a04e808c9f1458036c87dcc34a
yurieashi <yurieaishi@outlook.jp>
2026-07-08 02:00:03 +0900

    Refactor to use ES6 module syntax in issues.js

--- src/handlers/issues.js
+++ src/handlers/issues.js
@@ -1,10 +1,8 @@
-const { sendEmbed } =
-    require("../services/discord");
+import { sendEmbed } from '../services/discord.js';
 
-const buildEmbed =
-    require("../embeds/issueEmbed");
+import buildEmbed from '../embeds/issueEmbed.js';
 
-module.exports = async payload => {
+export default async (payload, env = {}) => {
     const action =
         payload.action;
 
@@ -23,5 +21,5 @@ module.exports = async payload => {
     const embed =
         buildEmbed(payload);
 
-    await sendEmbed(embed);
+    await sendEmbed(embed, undefined, env);
 };
