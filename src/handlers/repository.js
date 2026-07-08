a990c997b496ad34ee115e91343b4aa313b7ac5c
xyukuri <xyukuri@outlook.jp>
2026-07-09 00:14:38 +0800

    Add repository event handler to handle rename notifications

---
+++ src/handlers/repository.js
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2026 Kyouroga. https://kyouroga.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * Project: KyouLink
+ * Repository: https://github.com/Kyouroga/KyouLink
+ *
+ * For contribution guidelines, coding standards, and the pull request process,
+ * see CONTRIBUTING.md in the project root.
+ */
+
+// Handle repository events and emit rename notifications.
+import { sendEmbed } from '../services/discord.js';
+
+import buildEmbed from '../embeds/genericEmbed.js';
+
+export default async (payload, env = {}) => {
+    if (payload.action !== 'renamed') {
+        return;
+    }
+
+    const embed = buildEmbed(payload, 'repository');
+
+    if (!embed) {
+        return;
+    }
+
+    await sendEmbed(embed, undefined, env);
+};
